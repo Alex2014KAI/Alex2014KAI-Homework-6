d44c22e0525eb136956d6e506f7ecadbb86992d2
Alexei Petrov <petrovAlekse95@mail.ru>
2025-06-06 14:14:48 +0300

    Implemented correct reading of data taking into account comments

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -6,26 +6,8 @@ int main()
 {
    INIPARSER::INI_Parser parser("filename.txt");
     
-    //std::ifstream  file{};
-    //file.open("filename.txt");
 
-    //char ch;
-    //if (file.is_open()) {
 
-    //    while (file.get(ch)) { // считываем по одному символу
-    //        std::cout << ch << std::endl;   // выводим символ
-    //    }
-    //}
-    
-    /*
-    std::vector<int> a;
-    a.push_back(5);
-    a.push_back(5);
-    a.push_back(5);
-    a[0] = 2;
-
-    std::cout << a[0];
-    */
    for (auto vectorIt = parser.section.begin(); vectorIt != parser.section.end(); vectorIt++) {
        std::cout << "Section: " << (*vectorIt).getNameSection() << std::endl;
        for (auto mapIt = (*vectorIt).values_.begin(); mapIt != (*vectorIt).values_.end(); mapIt++) {
@@ -33,6 +15,8 @@ int main()
        }
        std::cout << "_______________________________" << std::endl;
   }
+
+
 
     return 1;
 }
--- INI_Parser.h
+++ INI_Parser.h
@@ -48,18 +48,28 @@ namespace INIPARSER {
 	private:
 		bool is_readingSection;  // cocked when section reading occurs
 		bool is_readingVariable; // cocked when data is read
-		bool is_readingNameVariable;
-		bool is_readingValueVariable;
-		bool is_readingComment;  // is cocked when the comment is read
+		bool is_readingNameVariable; // is set when a variable name is read
+		bool is_readingValueVariable; // is set when a variable value is read
+		bool is_readingComment;  // cocked when the comment is being read
 
 		std::string nameSection_;  // current section
 		std::string nameVariable;  // current variable name
 		std::string valueVariable; // current value of the variable
 		//std::map<std::string, std::string> temporaryValues{}; // stores data for the current section
 
-		Section debugData; // Хранит временные данные
+		Section debugData; // Stores temporary data for a section
+
+		bool isBlank(const std::string& s) { // if there is a symbol then it is false, if there are only spaces then it is true
+			for (char ch : s) {
+				if (!std::isspace(static_cast<unsigned char>(ch))) {
+					return false;
+				}
+			}
+			return true; 
+		}
+
 		void readDataSection() {
-			bool is_duplicate = false; // взводится если уже имеется секция с таким именем
+			bool is_duplicate = false; // is raised if there is already a section with the same name
 			for (int i{ 0 }; i < section.size(); i++) {
 				if (section[i].getNameSection() == nameSection_) {
 
@@ -80,33 +90,32 @@ namespace INIPARSER {
 			}
 		}
 
-		int (INI_Parser::*_execute)(char data); // depending on the variables: is_readingSection, is_readingVariable, is_readingComment will execute methods: readSectionName, ?? ??
+		int (INI_Parser::*_execute)(char data); // depending on the variables: is_readingSection, is_readingVariable, is_readingComment will execute methods: readSectionName, readVariable readComment
 
-		int execute(char data) {
+		int execute(char data) {                // the main method for data processing
 			return (this->*_execute)(data);
 		};
 
 		int readSectionName(char data) {
-			if (data == ']') { // section name ending
+			if (data == ']') {				   // section name ending
 				is_readingVariable = true;
 				is_readingSection = false;
 				is_readingComment = false;
 				return 1;
 			}
 			else if (data == '[') {
-				return 0; // ERRor
+				return 0;						// ERRor
 			}
 			else if (data == ';') {
-				return 0; // ERRor
+				return 0;						// ERRor
 			}
 			else {
 				nameSection_ += data;
 			}
-
-			return 1; // if return 0 - ERROR
+			return 1;							// if return 0 - ERROR
 		};
 
-		int readVariable(char data) {
+		int readVariable(char data) {			// reading data
 			if (data == ';') {
 				is_readingComment = true;
 				is_readingSection = false;
@@ -129,8 +138,6 @@ namespace INIPARSER {
 				nameSection_ = "";
 				nameVariable = "";
 				valueVariable = "";
-
-
 				return 1;
 			} 
 			else if (data == '=') {
@@ -139,11 +146,7 @@ namespace INIPARSER {
 				return 1;
 			}
 			else if (data == '\n') {
-				
-
-				// if (valueVariable != "" && nameVariable != "") { debugData.setValue(nameVariable, valueVariable); }; // условие не корректное 
-				if (nameVariable != "") { debugData.setValue(nameVariable, valueVariable); };
-
+				if (!isBlank(nameVariable)) { debugData.setValue(nameVariable, valueVariable); };
 				is_readingNameVariable = true;
 				is_readingValueVariable = false;
 				nameVariable = "";
@@ -153,9 +156,10 @@ namespace INIPARSER {
 			else {
 				if (is_readingNameVariable) {
 					nameVariable += data;
+					return 1;
 				}
 				else {
-					valueVariable += data; // не считывается последняя буква
+					valueVariable += data;
 					return 1;
 				}
 				return 1;
@@ -166,9 +170,13 @@ namespace INIPARSER {
 		};
 
 		int readComment(char data) {
-				if (file.peek() == '\n') {
-				std::cout << "perenos stroki" << std::endl;
+				if (data == '\n') {
+				// std::cout << "perenos stroki" << std::endl;   // for debugging
 				_execute = &INI_Parser::expectation;
+				is_readingSection = false;
+				is_readingComment = false;
+				is_readingVariable = false;
+				return 1;
 			}
 
 			return 1; // if return 0 - ERROR
@@ -176,49 +184,43 @@ namespace INIPARSER {
 
 		int expectation(char data) {
 
-			if (data == '[') { // Запись данных в общий вектор
-				
-				//bool is_duplicate = false; // взводится если уже имеется секция с таким именем
-				//	for (int i{ 0 }; i < section.size(); i++) {
-				//		if (section[i].getNameSection() == nameSection_) {
-
-				//			debugData.setNameSection(nameSection_);
-				//			for (auto mapIt = temporaryValues.begin(); mapIt != temporaryValues.end(); mapIt++) {
-				//				debugData.setValue(mapIt->first, mapIt->second);
-				//			}
-				//			section[i] = debugData;
-
-				//			is_duplicate = true;
-				//			break;
-				//		}
-				//	}
-				//	if (!is_duplicate) {
-				//		debugData.setNameSection(nameSection_);
-				//		for (auto mapIt = temporaryValues.begin(); mapIt != temporaryValues.end(); mapIt++) {
-				//			debugData.setValue(mapIt->first, mapIt->second);
-				//		}
-				//		section.push_back(debugData);
-				//		is_duplicate = false;
-				//	}
-				//	
-				//nameSection_ = "";
-				//nameVariable = "";
-				//valueVariable = "";
-				//temporaryValues.clear();
+			if (data == '[') { 
 
 				is_readingSection = true;
-				is_readingVariable = false;
 				is_readingComment = false;
+				is_readingVariable = false;
+
+				is_readingNameVariable = true;
+				is_readingValueVariable = false;
+
+				readDataSection();
+
+				nameSection_ = "";
+				nameVariable = "";
+				valueVariable = "";
+				return 1;
 			}
 			else if (data == ';') {
 				is_readingComment = true;
 				is_readingSection = false;
 				is_readingVariable = false;
+				return 1;
+			}
+			else if (data == '\n') {
+				return 1;
 			}
 			else {
+				if (!isBlank(nameVariable)) { debugData.setValue(nameVariable, valueVariable); };
+				is_readingNameVariable = true;
+				is_readingValueVariable = false;
+				nameVariable = "";
+				valueVariable = "";
+
+				nameVariable += data;             // so as not to lose 1 character in the variable name
 				is_readingVariable = true;
 				is_readingComment = false;
 				is_readingSection = false;
+				return 1;
 			}
 
 			return 1; // if return 0 - ERROR
@@ -242,7 +244,6 @@ namespace INIPARSER {
 				if (str == ';') {
 					_execute = &INI_Parser::readComment;
 				};
-				// execute(str);
 
 				while (file.get(str)){
 					if (is_readingSection) {
@@ -256,23 +257,14 @@ namespace INIPARSER {
 					};
 
 					execute(str);
-
-					/*
-					if (str == "[") {
-						is_readingSection = true;
-						is_readingVariable = false;
-						is_readingComment = false;
-					};
-					if (str == ";") {
-						is_readingComment = true;
-						is_readingSection = false;
-						is_readingVariable = false;
-					}
-					*/
-
 				};
 
-				// Записать еще раз все данные, потому что последняя секция не будет записана
+				if (file.eof()) {
+					if (nameVariable != "") { // Write the last variable if it exists
+						if (!isBlank(nameVariable)) { debugData.setValue(nameVariable, valueVariable); };
+						readDataSection();
+					}; 
+				}
 			}
 
 			file.close();
